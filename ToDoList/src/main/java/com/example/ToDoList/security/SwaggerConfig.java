fc1ba61487c0a8391c21d565dfb47295bc058faf
devch <chda2022@gmail.com>
2024-10-22 18:35:57 +0200

    Se agrega dependencia de swagger al pom, se permite la configuracion en el security y se agrega una nueva clase especial para el swagger

---
+++ ToDoList/src/main/java/com/example/ToDoList/security/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.ToDoList.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.ToDoList")) // Cambia al paquete correcto
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
